8b3b78622098ff8e7de3bb1477ed783303f0c84e
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-22 22:41:05 +0200

    Add 3x3 determinant

--- Pubc/Matrix Operations.h
+++ Pubc/Matrix Operations.h
@@ -18,7 +18,16 @@ namespace Math {
 	template<typename scalar>
 	inline scalar Determinant(scalar x0y0, scalar x1y0,
 							  scalar x0y1, scalar x1y1) {
-		return x0y0 * x1y1 - x1y0 * x0y1;
+		return (x0y0 * x1y1) - (x1y0 * x0y1);
+	}
+	
+	template<typename scalar>
+	inline scalar Determinant(scalar x0y0, scalar x1y0, scalar x2y0,
+							  scalar x0y1, scalar x1y1, scalar x2y1,
+							  scalar x0y2, scalar x1y2, scalar x2y2) {
+		return   x0y0 * (x1y1 * x2y2 - x2y1 * x1y2)
+			   - x1y0 * (x0y1 * x2y2 - x2y1 * x0y2)
+			   + x2y0 * (x0y1 * x1y2 - x1y1 * x0y2);
 	}
 
 	template<typename scalar, int dimensionality>
@@ -29,6 +38,11 @@ namespace Math {
 		if (dimensionality == 2) {
 			return Determinant(dat[0], dat[1],
 							   dat[2], dat[3]);
+		}
+		if (dimensionality == 3) {
+			return Determinant(dat[0], dat[1], dat[2],
+							   dat[3], dat[4], dat[5],
+                               dat[6], dat[7], dat[8]);
 		}
 	}
 
