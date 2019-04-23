0d942167c59c663a2174fc4f687d00bc06a2ca4d
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-23 16:08:43 +0200

    Add identity matrix to matrix operations

--- Pubc/Matrix Operations.h
+++ Pubc/Matrix Operations.h
@@ -50,5 +50,73 @@ namespace Math {
 		}
 	}
 
+    template<typename mat,
+             typename = std::enable_if_t<mat::Is_Square>>
+    inline mat & Set_Identity(mat & in)
+    {
+        auto & tup = in.as_tuple();
+
+        if (mat::Row_Count == 1) {
+            tup[0] = 1.f;
+            return in;
+        }
+        if (mat::Row_Count == 2) {
+            tup[0] = 1.f;
+            tup[1] = 0.f;
+            tup[2] = 0.f;
+            tup[3] = 1.f;
+            return in;
+        }
+        if (mat::Row_Count == 3) {
+            tup[0] = 1.f;
+            tup[1] = 0.f;
+            tup[2] = 0.f;
+            tup[3] = 0.f;
+            tup[4] = 1.f;
+            tup[5] = 0.f;
+            tup[6] = 0.f;
+            tup[7] = 0.f;
+            tup[8] = 1.f;
+            return in;
+        }
+        if (mat::Row_Count == 4) {
+            tup[ 0] = 1.f;
+            tup[ 1] = 0.f;
+            tup[ 2] = 0.f;
+            tup[ 3] = 0.f;
+            tup[ 4] = 0.f;
+            tup[ 5] = 1.f;
+            tup[ 6] = 0.f;
+            tup[ 7] = 0.f;
+            tup[ 8] = 0.f;
+            tup[ 9] = 0.f;
+            tup[10] = 1.f;
+            tup[11] = 0.f;
+            tup[12] = 0.f;
+            tup[13] = 0.f;
+            tup[14] = 0.f;
+            tup[15] = 1.f;
+            return in;
+        }
+
+        int indx = 0;
+        for (int y = 0; y < mat::Row_Count; y++) {
+            for (int x = 0; x < mat::Column_Count; x++, indx++) {
+                tup[indx] = (x == y)? 1.f : 0.f;
+            }
+        }
+        
+        return in;
+    }
+
+    template<typename mat,
+             typename = std::enable_if_t<mat::Is_Square>>
+    inline mat Identity()
+    {
+        mat ret;
+        Set_Identity(ret);
+        return ret;
+    }
+
 }
 }
