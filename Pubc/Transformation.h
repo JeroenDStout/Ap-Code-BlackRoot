f9747489ecdccc917fbd89d357a341d8053d4da6
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-23 20:32:01 +0200

    Fix descriptions and clarify for translated map

--- Pubc/Transformation.h
+++ Pubc/Transformation.h
@@ -17,15 +17,15 @@ namespace Math {
         using ScalarType         = typename map::ScalarType;
         using TranslatedMapType  = TranslatedMap<map, translate>;
         
-        static_assert(Math::has_equivalent_matrix_type<map>::value, "Map must be representable as a square matrix");
+        static_assert(Math::has_equivalent_matrix_type<map>::value, "Map must be representable as a matrix");
         static_assert(Math::has_vector_type<translate>::value, "Translation must be a vector");
-        static_assert(map::OperatingDimensionality == translate::OperatingDimensionality, "Operating dimensionality of map and translation must be equal");
-        static_assert(map::EquivalentMatrixType::IsSquare, "Map must be representable as a square matrix");
+        static_assert(map::Operating_Dimensionality == translate::Operating_Dimensionality, "Operating dimensionality of map and translation must be equal");
+        static_assert(map::EquivalentMatrixType::Is_Square, "Map must be representable as a square matrix");
 
             // -- Type
         
         static const size_t Size                     = map::Size + translate::Size;
-        static const size_t OperatingDimensionality  = map::OperatingDimensionality;
+        static const size_t Operating_Dimensionality = map::Operating_Dimensionality;
 
             // -- Members
 
