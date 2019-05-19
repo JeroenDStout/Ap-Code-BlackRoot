d66041829c99800f351c2372797cd022e9a845ec
Jeroen D Stout <jeroenstout@gmail.com>
2019-05-19 22:24:55 +0200

    Improve hash id / add hash name

--- Pubc/Hash ID.h
+++ Pubc/Hash ID.h
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
- /*
-  */
-
 #pragma once
 
 #include "BlackRoot/Pubc/UUID.h"
@@ -13,21 +10,52 @@
 namespace BlackRoot {
 namespace Identify {
 
-    namespace Detail {
+    struct HashSugar128 {
+        char data0[19];
+        char data1[19];
+        char data2[19];
+        char data3[19];
 
-        constexpr auto fnv1a_128(char const * s, std::size_t count) -> uint128 {
-            const uint128 prime( 0x0000000001000000, 0x000000000000013B);
-            const uint128 offset(0x6c62272e07bb0142, 0x62b821756295c58d);
-
-            uint128 ret = offset;
-            for (std::size_t i = 0; i < count; i++) {
-                ret *= prime;
-                ret ^= s[count];
-            }
-
-            return ret;
+        constexpr HashSugar128(uint128 hash)
+        : data0(), data1(), data2(), data3() {
+            this->set_data(0, uint32(hash >>  0));
+            this->set_data(1, uint32(hash >> 32));
+            this->set_data(2, uint32(hash >> 64));
+            this->set_data(3, uint32(hash >> 96));
         }
 
+        constexpr void set_data(size_t i, uint32 value) {
+            char * data = (*this)[i];
+            data[0] = '0';
+            data[1] = 'x';
+            for (int d = 0; d < 16; d++) {
+                data[2+d] = '0' + (uint8(value >> (d*4)) & 0xF);
+            }
+            data[18] = 0;
+        }
+
+        constexpr char * operator[](size_t i) {
+            switch (i) {
+              default: return nullptr;
+              case 0: return data0;
+              case 1: return data1;
+              case 2: return data2;
+              case 3: return data3;
+            };
+        }
+    };
+
+    constexpr auto fnv1a_128(char const * s, std::size_t count) -> uint128 {
+        const uint128 prime( 0x0000000001000000, 0x000000000000013B);
+        const uint128 offset(0x6c62272e07bb0142, 0x62b821756295c58d);
+
+        uint128 ret = offset;
+        for (std::size_t i = 0; i < count; i++) {
+            ret *= prime;
+            ret ^= s[count];
+        }
+
+        return ret;
     }
 
     inline namespace Literals {
@@ -35,7 +63,7 @@ namespace Identify {
 
             constexpr uint128 operator "" _fnv1a_128(const char * s, std::size_t n)
             {
-                return Detail::fnv1a_128(s, n);
+                return fnv1a_128(s, n);
             }
 
         }
---
+++ Pubc/Hash Name.h
@@ -0,0 +1,69 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "BlackRoot/-genc/Hash Names.h"
+#include "BlackRoot/Pubc/Number Types.h"
+
+namespace BlackRoot {
+namespace Identify {
+
+    struct HashName32 {
+        char name[
+          Tables::Adjective::max_length + 1 +
+          Tables::First::max_length + 1 +
+          Tables::Family::max_length + 2 +
+          Tables::City::max_length + 1
+        ];
+
+        constexpr HashName32()
+          : name() {}
+
+        constexpr char const * get() {
+            return name;
+        }
+    };
+
+    constexpr HashName32 generate_hash_name_32(uint32 data) {
+        HashName32 ret;
+
+        char const * adj    = Tables::Adjective::get( 0xFF & (data >> 24) );
+        char const * first  = Tables::First::get(     0xFF & (data >> 16) );
+        char const * family = Tables::Family::get(    0xFF & (data >>  8) );
+        char const * city   = Tables::City::get(      0xFF & (data >>  0) );
+
+        size_t write_point = 0;
+        char const * parcer = 0;
+        
+        parcer = adj;
+        while (parcer[0] != 0) {
+            ret.name[write_point++] = *(parcer++);
+        }
+        ret.name[write_point++] = ' ';
+        
+        parcer = first;
+        while (parcer[0] != 0) {
+            ret.name[write_point++] = *(parcer++);
+        }
+        ret.name[write_point++] = ' ';
+        
+        parcer = family;
+        while (parcer[0] != 0) {
+            ret.name[write_point++] = *(parcer++);
+        }
+        ret.name[write_point++] = ',';
+        ret.name[write_point++] = ' ';
+        
+        parcer = city;
+        while (parcer[0] != 0) {
+            ret.name[write_point++] = *(parcer++);
+        }
+        ret.name[write_point++] = 0x0;
+
+        return ret;
+    }
+
+}
+}
